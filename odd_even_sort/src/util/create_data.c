0cf35bd95f3905aa585d2cafbe8beb749f87ac68
DreamingLee <dreaming_lee@outlook.com>
2019-06-04 17:38:42 +0800

    change the way of output of create_data

--- odd_even_sort/src/util/create_data.c
+++ odd_even_sort/src/util/create_data.c
@@ -10,18 +10,15 @@
 */
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: program <size_of_array_to_be_sorted>\n");
+        printf("Usage: program <size_of_array_to_be_sorted> > <filename>\n");
         exit(1);
     }
     int n = atoi(argv[1]);
     int *rand_array = create_rand_array(n);
-    FILE *fp = NULL;
-    fp = fopen("./data.txt", "w");
-    fprintf(fp, "%d\n", n);
+    printf("%d\n", n);
     for (int i = 0; i < n; i++) {
-        fprintf(fp, "%d\n", rand_array[i]);
+        printf("%d\n", rand_array[i]);
     }
-    fclose(fp);
     free(rand_array);
 
     return 0;
